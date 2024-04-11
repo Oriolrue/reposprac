ed8b2d72344b679889584300773f3e8dd105e8d6
daw1 <orue@elpuig.xeill.net>
2024-04-11 19:51:13 +0200

    Added methods for collaborators

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,22 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        mayla();
+        alejandro();
+        mouad();
+        oriol();
+
+    }
+    private static void mayla(){
+
+    }
+    private static void alejandro(){
+
+    }
+    private static void mouad(){
+
+    }
+    private static void oriol(){
+
     }
 }
